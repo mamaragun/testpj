2551165c8b7d712112985c313f922673067cc518
poagreb <poa.greb@gmail.com>
2014-05-30 14:47:12 +0700

    add bg

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<html>
+	 <body style="background:  url('http://f.ptcdn.info/453/009/000/1378645129-373b47d37d-o.jpg') no-repeat fixed center;   -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;" >
+	</body>
+</html>
+
 
 <?php
 	echo "php version 5.3";
