206718f2396d09342ea6ecbe6b9f32147abf3716
ArnonW <arnonw.my@gmail.com>
2014-05-31 12:05:37 +0700

    update

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-<!--  fdfdfdf -->
-=======
->>>>>>> 7263f1b527fa944b87c122a3875348aacc1829aa
 <?php
 	echo "php version 5.3";
 	echo "Ratchaprasong";
 	echo "test drone";
-	echo phpinfo();
-
-
-
 	echo "<img src='http://f.ptcdn.info/453/009/000/1378645129-373b47d37d-o.jpg'/>";
 
 
