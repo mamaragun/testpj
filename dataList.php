b524731023b90656f1a54b9e4ae2b01ec867e537
mamaragun <montree402@gmail.com>
2014-05-31 12:48:37 +0700

    edit error datlistfile

--- dataList.php
+++ dataList.php
@@ -2,15 +2,17 @@
 header ('Content-type: text/html; charset=utf-8');
 include "config.php";
 
-$SQL=mysql_query("SELECT firstname , lastname , nickname , picture , comment FROM commentator");
+$SQL= "SELECT firstname , lastname , nickname , picture , comment FROM commentator";
 
 echo "<table class='table table-border'>";
 
 $objQuery = mysql_query($SQL) or die(mysql_error);
-while($row=mysql_fetch_array($objQuery){
+while($row = mysql_fetch_array($objQuery))
+
+{
 echo "<tr>";
 echo "<td>".$row['firstname']."</td>";
-ehco "</tr>";
+echo "</tr>";
 }
 
 echo "</table>";
