f42f3073be1f4df513b38fb51983baccd623065d
ArnonW <arnonw.my@gmail.com>
2014-05-31 15:36:52 +0700

    update

--- dataList.php
+++ dataList.php
@@ -10,25 +10,24 @@ $objQuery = mysql_query($SQL) or die(mysql_error);
 while($row = mysql_fetch_array($objQuery))
 {
 
-echo "<div class="jumbotron"><div class ="big_fram"style="" align="center"   ><div class= "big_face">".
-"<img style='width:300px; height;200px;' src='".$row['picture']."'>".
-"<img src='http://t2.gstatic.com/images?q=tbn:ANd9GcQr2sr9VcoijmctscWiRbJhLHYGuTSpT9N2kRD04fYgObKQbVwO'  style="margin-left:-80px;"/>".
+echo  "<div class='jumbotron'><div class ='big_fram' style='' align='center'   ><div class= 'big_face'>".
+"<img style=width:300px; height;200px; src=".$row['picture'].">".
   "</div>".
 "</div>".
 
  "<h2>".$row['firstname']." ".$row[lastname]." (".$row[nickname].")</h2>".
-"<br>". 
+"<br>".
 "<br>".
 
 "<div>".
   "<p>".
-"<a class="btn btn-danger btn-lg" role="button" style="" >คะแนน 8/10</a>".
-"<a class="btn btn-primary btn-lg" role="button" >Read more & Comment</a></p>".
+"<a class='btn btn-danger btn-lg' role='button' style='' >คะแนน 8/10</a>".
+"<a class='btn btn-primary btn-lg' role='button' >Read more & Comment</a></p>".
 "</div>".
 "</div>";
 
-
-/*
+?>
+*
 echo "<tr>";
 echo "<td><img style='width:300px; height;200px;' src='".$row['picture']."'></td>";
 echo "<td>".$row['firstname']."</td>";
