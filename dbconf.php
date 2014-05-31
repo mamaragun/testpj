31b0ad140178ad053227ee5eb23fc4cad8be7343
ArnonW <arnonw.my@gmail.com>
2014-05-31 16:19:31 +0700

    dbconf

--- dbconf.php
+++ dbconf.php
@@ -1,8 +1,16 @@
 <?
+
     $db_server_ip ="localhost";     //server_ip
     $db_username="ratchaprasong";        //user login
     $db_password="ratchaprasong";               //user password
     $db_name="ratchaprasong";              //database instant
+
+
+    $objConnect = mysql_connect ($dbhost, $dbuser, $dbpass) or die("Could not connect: " . mysql_error());
+    mysql_select_db($db_name) or die ("Can't connect ");
+mql_query("SET character_set_results=utf8");
+mysql_query("SET character_set_client =utf8");
+mysql_query("SET character_set_connection=utf8");
 
 ?>
 
