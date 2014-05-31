04804951f5d02eeef63984d1bb5d727fd417a4e2
poagreb <poa.greb@gmail.com>
2014-05-31 12:25:02 +0700

    add new video

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
       <meta charset="utf-8">
 
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
 	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 	<style>
   .big_fram {
@@ -88,14 +87,16 @@
 	<div class="jumbotron">
     <div class ="big_fram"style="float:left;"  >
       <div class= "big_face">
-    <img src='http://f.ptcdn.info/453/009/000/1378645129-373b47d37d-o.jpg'  style="margin-left:-80px;"/>
+    <img src='http://t2.gstatic.com/images?q=tbn:ANd9GcQr2sr9VcoijmctscWiRbJhLHYGuTSpT9N2kRD04fYgObKQbVwO'  style="margin-left:-80px;"/>
   </div>
   </div>
   	<h2>เอกราช เก่งทุกทาง (ฉายา)</h2>
 
   	<p >นักพากย์ คือผู้ที่ให้เสียงสำหรับภาพยนตร์เอะนิเมะชัน ภาพยนตร์ต่างประเทศ การ์ตูน ละครทีวี หรือวิทยุ นักพากย์การ์ตูน นักพากย์เสียงอาจจะใช้ฉายา ของตัวเองในการพากย์เพื่อให้ ผู้ฟังหรือผู้ชมจดจำได้ง่าย เช่น น้าต๋อยเซมเบ้ หลายครั้งที่นักพากย์เสียงไม่เป็นที่รู้จักของประชาชน เนื่องจากไม่ได้ปรากฏตัวให้ผู้อื่นจำหน้าได้ บางครั้งอาจให้นักแสดงที่มีชื่อเสียงมาเป็นนักพากย์ เพื่อให้ผู้ชมจำเสียงได้</p>
-    <p><a class="btn btn-primary btn-lg" role="button" style="margin-top:50px;">Read more</a></p>
-	</div>
+    <p><a class="btn btn-primary btn-lg" role="button" style="margin-top:50px;">Read more & Comment</a></p>
+	<div align="center"> <iframe width="560" height="315" src="//www.youtube.com/embed/nRsfC5ErY_c" frameborder="0" allowfullscreen ></iframe></div>
+
+  </div>
 
     </div>
 
